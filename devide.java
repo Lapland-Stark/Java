9471619cf8c756a0195e5b1e59bb6ed04bc1026e
Lapland-Stark <Ellie_Stark1984@163.com>
2020-02-15 16:04:48 +0800

    upload minus & devide

---
+++ devide.java
@@ -0,0 +1,11 @@
+public class devide
+{
+	float a = 21.2f;
+	int b = 0;
+	public static void main(String args[])
+	{
+		devide u = new devide();
+		float r = u.a/u.b;
+		System.out.println(r);
+	}
+}
---
+++ minus.java
@@ -0,0 +1,11 @@
+public class minus
+{
+	char c1 = 'f';
+	char c2 = 'e';
+	public static void main(String args[])
+	{
+		minus h1 = new minus();
+		int c = h1.c1 - h1.c2;
+		System.out.println(c);
+	}
+}
